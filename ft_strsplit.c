ca7af26e8c41162f9a13085616a624e2def39e1e
ssalaues <ssalaues@users.noreply.github.com>
2016-12-07 17:26:37 -0800

    strsplit semi functional

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -12,20 +12,26 @@
 
 #include "libft.h"
 
-char	**ft_strsplit(char const *s, char c)
+char			**ft_strsplit(const char *s, char c)
 {
 	char	**str;
-	size_t	i;
+	int		nb;
+	int		i;
 
 	i = 0;
-	while (*s)
+	nb = ft_wcount(s, c);
+	str = (char **)malloc(sizeof(*str) * (ft_wcount(s, c) + 1));
+	if (!str)
+		return (NULL);
+	while (nb--)
 	{
-		while (*s == c)
+		while (*s == c && *s != '\0')
 			s++;
-		if (*s != c)
-		{
-			i++;
-
-		}
+		str[i] = ft_strsub((const char *)s, 0, ft_wlen(s, c));
+		if (str[i] == NULL)
+			return (NULL);
+		s = s + ft_wlen(s, c);
+		i++;
 	}
+	return (str);
 }
